8af2f4a896a7d76ecb75defd62d51cd288a410de
[João Nogueira] <146846348+Jokinhas24@users.noreply.github.com>
2026-05-14 15:55:15 +0100

    Program to write on another file

---
+++ FilePower1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FilePower1
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Queue<string> stringToFile = new Queue<string>();
+
+            while(true)
+            {
+                Console.WriteLine("\nWrite something to add to the file: ");
+                Console.WriteLine("Or Press 'Enter' to finish");
+
+                string input = Console.ReadLine();
+                if(input == "")
+                    break;
+                else
+                {
+                    stringToFile.Enqueue(input);
+                }
+            }
+
+            Console.WriteLine("\nSaving in the file...");
+                foreach (string s in stringToFile)
+                {
+                    File.AppendAllText(args[0], s + "\n");
+                }
+        }
+    }
+}
